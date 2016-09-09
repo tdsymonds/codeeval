f168df8c97172e8d2b6940af39b9ab99219dc5e3
Tom Symonds <tdsymonds@hotmail.com>
2016-09-09 13:52:09 +0000

    Add solution for 34 number pairs python

---
+++ python/moderate/(34) number-pairs.py
@@ -0,0 +1,35 @@
+from collections import deque
+import sys
+
+def main(filepath):
+    with open(filepath, 'r') as f:
+        for line in f.readlines():
+            if line:
+                line = line.strip()
+                line = line.split(';')
+
+                x = int(line[1])
+                sorted_numbers = map(int, line[0].split(','))
+
+                pairs = []
+
+                for number in sorted_numbers:
+                    paired_number = x - number
+                    if number == paired_number and sorted_numbers.index(number) == sorted_numbers.index(paired_number):
+                        continue
+                    if paired_number in sorted_numbers:
+                        pairs.append(sorted([number, paired_number]))
+
+                if pairs:
+                    result_list = []
+                    for pair in pairs:
+                        pair_string = ','.join(map(str, pair))
+                        if pair_string not in result_list:
+                            result_list.append(pair_string)
+                    print ';'.join(result_list)
+                else:
+                    print 'NULL'
+
+
+if __name__ == '__main__':
+    main(sys.argv[1])
