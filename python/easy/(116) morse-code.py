96b884d1fa59c60327465060cc9e30963c9d0a3b
Tom Symonds <tdsymonds@hotmail.com>
2016-09-12 11:15:21 +0000

    Add solution to python 116 morse code

---
+++ python/easy/(116) morse-code.py
@@ -0,0 +1,61 @@
+import sys
+
+MORSE_CODE = {
+    '.-': 'A',
+    '-...': 'B',
+    '-.-.': 'C',
+    '-..': 'D',
+    '.': 'E',
+    '..-.': 'F',
+    '--.': 'G',
+    '....': 'H',
+    '..': 'I',
+    '.---': 'J',
+    '-.-': 'K',
+    '.-..': 'L',
+    '--': 'M',
+    '-.': 'N',
+    '---': 'O',
+    '.--.': 'P',
+    '--.-': 'Q',
+    '.-.': 'R',
+    '...': 'S',
+    '-': 'T',
+    '..-': 'U',
+    '...-': 'V',
+    '.--': 'W',
+    '-..-': 'X',
+    '-.--': 'Y',
+    '--..': 'Z',
+    '-----': '0',
+    '.----': '1',
+    '..---': '2',
+    '...--': '3',
+    '....-': '4',
+    '.....': '5',
+    '-....': '6',
+    '--...': '7',
+    '---..': '8',
+    '----.': '9',
+}
+
+def main(filepath):
+    with open(filepath, 'r') as f:
+        for line in f.readlines():
+            if line:
+                line = line.strip()
+                line = line.split(' ')
+                
+                result = ''
+
+                for code in line:
+                    if code:
+                        result += MORSE_CODE[code]
+                    else:
+                        result += ' '
+
+                print result
+
+
+if __name__ == '__main__':
+    main(sys.argv[1])
